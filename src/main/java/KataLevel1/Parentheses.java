418ea2ff23cf5fb3aa648044679281c3d5f1c82c
VikMoto <ig.basalt@gmail.com>
2022-12-16 17:16:31 +0200

    16.12.2022 Kata

--- src/main/java/KataLevel1/Parentheses.java
+++ src/main/java/KataLevel1/Parentheses.java
@@ -1,5 +1,20 @@
 package KataLevel1;
-
+/**Write a function that takes a string of parentheses, and determines if the order of
+// the parentheses is valid. The function should return true if the string is valid,
+// and false if it's invalid.
+//
+//Examples
+//"()"              =>  true
+//")(()))"          =>  false
+//"("               =>  false
+//"(())((()())())"  =>  true
+//Constraints
+//0 <= input.length <= 100
+//
+//Along with opening (() and closing ()) parenthesis, input may contain any valid ASCII
+// characters. Furthermore, the input string may be empty and/or not contain any
+// parentheses at all. Do not treat other forms of brackets as parentheses
+// (e.g. [], {}, <>).*/
 public class Parentheses {
     public static boolean validParentheses(String parens)
     {
@@ -16,7 +31,7 @@ public class Parentheses {
         return count == 0;
     }
     public static void main(String[] args) {
-        String str = "";
+        String str = ")(()))";
         System.out.println("validParentheses(str) = " + validParentheses(str));
     }
 }
